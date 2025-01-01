3a7fb5d9fe520b2044af05aff32f7b410bdd421b
devemre <dev.emre.aydin@gmail.com>
2025-01-01 17:19:14 +0300

    Added Validation

---
+++ src/main/java/com/devemre/msscbeerservice/repositories/BeerRepository.java
@@ -0,0 +1,9 @@
+package com.devemre.msscbeerservice.repositories;
+
+import com.devemre.msscbeerservice.domain.Beer;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.util.UUID;
+
+public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> {
+}
